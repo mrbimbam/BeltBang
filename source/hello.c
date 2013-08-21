1aa6edadfd2cbcf27c8eea17d057d53882771aef
Geir Anders Storsveen <storsveen@gmail.com>
2013-08-21 20:14:47 +0200

    new file:   source/hello.c

---
+++ source/hello.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  Just testing
+ *
+ *        Version:  1.0
+ *        Created:  08/21/2013 12:30:31 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Geir Anders Storsveen (gas), storsveen@gmail.com
+ *        Company:  N/A
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+	int
+main ( int argc, char *argv[] )
+{
+	printf("Hello, WorldMaster!!!!!!\n");
+
+	return 1;
+}				/* ----------  end of function main  ---------- */
